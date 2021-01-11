b06f7c26eeb42c44d1d389aab6dbf3efcc751de7
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-01-11 23:17:26 +0700

    Add Controller

---
+++ Client/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddData(Student model)
+        {
+            using (var context = new demoCRUDEntities())
+            {
+                context.Students.Add(model);
+                context.SaveChanges();
+            }
+            string message = "Add the record successfully";
+            ViewBag.Message = message;
+
+            return View();
+        }
+
+        [HttpGet] // Set the attribute to Read 
+        public ActionResult GetData()
+        {
+            using (var context = new demoCRUDEntities())
+            {
+                var data = context.Students.ToList();
+                return View(data);
+            }
+        }
+    }
+}
+}
